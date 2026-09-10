bc32f244d152c6d29c4b0cd71ceb6669a4b7a2eb
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-09-10 16:30:40 +0100

    Run black formatting

--- arches_portico/celery.py
+++ arches_portico/celery.py
@@ -7,7 +7,7 @@ import platform
 if platform.system().lower() == "windows":
     os.environ.setdefault("FORKED_BY_MULTIPROCESSING", "1")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arches_portico.settings')
-app = Celery('arches_portico')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arches_portico.settings")
+app = Celery("arches_portico")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
